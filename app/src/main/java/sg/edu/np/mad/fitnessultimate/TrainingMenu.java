39f80b413e52ac5cf3dc7d8d8da677caa5ff1f42
gnay-uy <87360806+gnay-uy@users.noreply.github.com>
2024-05-17 13:07:46 +0800

    feat: set up intents for redirection to appropriate activities from training menu

--- app/src/main/java/sg/edu/np/mad/fitnessultimate/TrainingMenu.java
+++ app/src/main/java/sg/edu/np/mad/fitnessultimate/TrainingMenu.java
@@ -1,6 +1,9 @@
 package sg.edu.np.mad.fitnessultimate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,28 @@ public class TrainingMenu extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button followAlongWorkouts = findViewById(R.id.followAlongWorkouts);
+        Button exercises = findViewById(R.id.exercises);
+
+        followAlongWorkouts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TrainingMenu.this, FollowAlongWorkouts.class);
+                startActivity(intent);
+            }
+        });
+
+        exercises.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TrainingMenu.this, Exercises.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+
 }
