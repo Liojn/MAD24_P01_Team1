40fd874417279be158d227283a3baf859a5a294b
Liojn <s10259865@connect.np.edu.sg>
2024-05-29 13:20:28 +0800

    gang

--- app/src/main/java/sg/edu/np/mad/fitnessultimate/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/fitnessultimate/MainActivity.java
@@ -1,4 +1,4 @@
-package sg.edu.np.mad.fitnessultimate.activity;
+package sg.edu.np.mad.fitnessultimate;
 
 import android.os.Bundle;
 
@@ -7,8 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import sg.edu.np.mad.fitnessultimate.R;
 
 public class MainActivity extends AppCompatActivity {
 
