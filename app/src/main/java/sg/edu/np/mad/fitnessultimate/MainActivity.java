06204e682fce1d25cb14763dba26f9ad2f2cad8a
Kiefer <S10259865@connect.np.edu.sg>
2024-07-28 00:24:59 +0800

    Style: Trying to add cards in home page

--- app/src/main/java/sg/edu/np/mad/fitnessultimate/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/fitnessultimate/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView bannerRecyclerView;
     private BannerAdapter bannerAdapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,88 +71,67 @@ public class MainActivity extends AppCompatActivity {
 
         welcomeTextView = findViewById(R.id.welcomeTextView);
 
-
         profileBtn = findViewById(R.id.profileButton);
 
         if (user == null) {
-            // User is not logged in, navigate to LoginOrSignUpOption
             Intent intent = new Intent(MainActivity.this, LoginOrSignUpOption.class);
             startActivity(intent);
             finish();
-            return; // Early return to prevent further execution
+            return;
         }
 
-        // Retrieve and set the username
         retrieveAndSetUsername();
 
         changeProfilePic = findViewById(R.id.profileButton);
 
-        // Retrieve the profile image URI from SharedPreferences
         SharedPreferences prefs = getSharedPreferences("profile", MODE_PRIVATE);
         String profileImageUri = prefs.getString("profileImageUri", null);
         if (profileImageUri != null) {
-            // Set profile picture using URI
             changeProfilePic.setImageURI(Uri.parse(profileImageUri));
         } else {
-            // If no profile image URI is found in SharedPreferences, retrieve it from Firestore
             retrieveProfileImage();
         }
 
-        // Handle the result from EditProfilePageActivity
         if (getIntent().hasExtra("profileImageUri")) {
-            // Get the updated profile picture URI from the result intent
             String updatedProfileImageUri = getIntent().getStringExtra("profileImageUri");
-            // Set the profile picture using the updated URI
             changeProfilePic.setImageURI(Uri.parse(updatedProfileImageUri));
         }
 
-        // Set OnClickListener for the profile icon to navigate to ProfilePageActivity
         ImageView profileIcon = findViewById(R.id.profileButton);
-        profileIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ProfilePageActivity.class);
-                startActivity(intent);
-            }
+        profileIcon.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ProfilePageActivity.class);
+            startActivity(intent);
         });
 
-        //For Onclick for Training Schedule
         findViewById(R.id.nav_fitness).setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, TrainingMenuActivity.class);
             startActivity(intent);
         });
-        //For Onclick for Chatbot
+
         findViewById(R.id.chat_button).setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, ChatbotActivity.class);
             startActivity(intent);
         });
-        //For Onclick for Calendar
+
         findViewById(R.id.nav_calendar).setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
             startActivity(intent);
         });
-        //For Onclick for FoodTracker
+
         findViewById(R.id.nav_food).setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, FoodTracker.class);
             startActivity(intent);
         });
 
-        //For Onclick for HomePage
         findViewById(R.id.nav_home).setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
         });
 
-        //
-
-
-        // Recycler View inside Banner for homepage
-        bannerRecyclerView = findViewById(R.id.bannerRecyclerView);
-        //List of banner images + title + subtitles
         List<BannerItem> itemList = Arrays.asList(
                 new BannerItem(R.drawable.discover_excersies, "Discover Exercises", "Click Here!", TrainingMenuActivity.class),
-                new BannerItem(R.drawable.benefits_excerise, null, null   , null)
+                new BannerItem(R.drawable.benefits_excerise, null, null, null)
         );
 
         bannerAdapter = new BannerAdapter(this, itemList);
@@ -159,31 +139,24 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         bannerRecyclerView.setLayoutManager(layoutManager);
         bannerRecyclerView.setAdapter(bannerAdapter);
-
     }
 
     private void retrieveProfileImage() {
         if (user != null) {
-            // Retrieve the user document from Firestore
             DocumentReference userRef = fStore.collection("users").document(user.getUid());
             userRef.get().addOnSuccessListener(documentSnapshot -> {
                 if (documentSnapshot.exists()) {
-                    // Extract the image URL from the user document
                     String imageUrl = documentSnapshot.getString("profileImageUrl");
-
-                    // Load the image into ImageView using Glide
                     Glide.with(this)
                             .load(imageUrl)
-                            .placeholder(R.drawable.baseline_account_circle_24) // Placeholder image while loading
+                            .placeholder(R.drawable.baseline_account_circle_24)
                             .fitCenter()
                             .transform(new CircleCrop())
                             .into(profileBtn);
                 } else {
-                    // Handle case where user document does not exist
                     Toast.makeText(MainActivity.this, "User document does not exist.", Toast.LENGTH_SHORT).show();
                 }
             }).addOnFailureListener(e -> {
-                // Handle any errors that occur while fetching user document
                 Toast.makeText(MainActivity.this, "Failed to retrieve profile image: " + e.getMessage(), Toast.LENGTH_SHORT).show();
             });
         }
@@ -194,19 +167,15 @@ public class MainActivity extends AppCompatActivity {
             DocumentReference userRef = fStore.collection("users").document(user.getUid());
             userRef.get().addOnSuccessListener(documentSnapshot -> {
                 if (documentSnapshot.exists()) {
-                    // Extract the username from the user document
                     String username = documentSnapshot.getString("uName");
                     if (username != null) {
-                        // Update the welcome text with the retrieved username
                         String welcomeText = "Welcome back, " + username + "!\nWhat would you like to do today?";
                         welcomeTextView.setText(welcomeText);
                     }
                 } else {
-                    // Handle case where user document does not exist
                     Toast.makeText(MainActivity.this, "User document does not exist.", Toast.LENGTH_SHORT).show();
                 }
             }).addOnFailureListener(e -> {
-                // Handle any errors that occur while fetching user document
                 Toast.makeText(MainActivity.this, "Failed to retrieve username: " + e.getMessage(), Toast.LENGTH_SHORT).show();
             });
         }
